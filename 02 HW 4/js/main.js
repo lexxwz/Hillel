2e13da4ac3da035511ad0e48c817474e3147ecea
Eugen <63226510+lexxwz@users.noreply.github.com>
2023-09-26 15:41:32 +0300

    Update main.js

--- 02 HW 4/js/main.js
+++ 02 HW 4/js/main.js
@@ -3,6 +3,8 @@ let hours = Number(prompt('Введіть кількість годин:'));
 if (hours) {
     let seconds = hours * 3600;
     alert(`Кількість секунд у ${hours} годині(ах) - ${seconds}.`);
+} else if (hours === 0) {
+    alert('Ви ввели 0.');
 } else {
     alert('Ви ввели не число.');
 }
